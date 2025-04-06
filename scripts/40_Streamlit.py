143b9c9f6a2512bdf631dd19cee7b6e9b55cec15
BasaJess <email@jesusbasail.com>
2025-04-06 10:41:05 +0200

    04061040

--- scripts/40_Streamlit.py
+++ scripts/40_Streamlit.py
@@ -5,4 +5,7 @@ st.title("Neuefische School and Pool for digital talent")
 
 with st.chat_message("assistant"):
     st.write("Hello")
-    st.bar_chart(np.random.randn(30, 3))
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
