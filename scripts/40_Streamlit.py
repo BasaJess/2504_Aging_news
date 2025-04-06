692f2edbe5a8e2e19263822e16f0b08629871b8e
BasaJess <email@jesusbasail.com>
2025-04-06 11:24:05 +0200

    04061123

--- scripts/40_Streamlit.py
+++ scripts/40_Streamlit.py
@@ -3,9 +3,6 @@ import numpy as np
 
 st.title("Neuefische School and Pool for digital talent")
 
-with st.chat_message("assistant"):
-    st.write("Hello")
-
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -15,6 +12,16 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
