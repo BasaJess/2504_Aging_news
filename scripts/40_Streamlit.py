93dad013bd62fee53cde0b184f3a3bc2fd7e39e4
BasaJess <email@jesusbasail.com>
2025-04-06 11:06:03 +0200

    040611105

--- scripts/40_Streamlit.py
+++ scripts/40_Streamlit.py
@@ -6,6 +6,15 @@ st.title("Neuefische School and Pool for digital talent")
 with st.chat_message("assistant"):
     st.write("Hello")
 
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
