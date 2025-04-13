c8bf5e2a073807f5e5e4bf70b095eaab926466a1
BasaJess <email@jesusbasail.com>
2025-04-13 18:42:11 +0200

    04131842

---
+++ scripts/34_Most_relevants_docs_list_maker.py
@@ -0,0 +1,8 @@
+# This one received a df with the cleaned df containing the info of the docs
+# orders the list first by relevance and then by date
+# returns the list in df
+# it should be ready to be displayed to the user in a web interface 
+
+
+def most_relevant_docs_list(df):
+    pass
