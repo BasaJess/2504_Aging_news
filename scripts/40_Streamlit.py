a69fed00411932be4e0aa74700054277edbf72c8
BasaJess <email@jesusbasail.com>
2025-04-14 11:53:46 +0200

    04141153

--- scripts/40_Streamlit.py
+++ scripts/40_Streamlit.py
@@ -61,7 +61,7 @@ with st.sidebar:
     if st.button("Click me :)"):
         # call the most relevant documents
         # df = retrieve_most_relevant_docs_for_streamlit()
-        df = pd.read_csv("topdocs.csv")
+        df = pd.read_json("top_10_docs.json")
         # display the df
         st.dataframe(df,use_container_width=True)
         simulated_input = "What time is in London?"# ask_30_LLM_latest_findings()
